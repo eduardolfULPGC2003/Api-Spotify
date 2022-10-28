66c27dd7a90617b86b08d70154352f050bb486fc
eduardolfULPGC2003 <eduardolf2003@gmail.com>
2022-10-28 09:32:01 +0100

    First commit

---
+++ src/main/java/es/ulpgc/spotify/downloader/Main.java
@@ -0,0 +1,19 @@
+package es.ulpgc.spotify.downloader;
+
+import java.util.Collections;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        SpotifyAccessor accessor = new SpotifyAccessor();
+        String json = accessor.get("/albums/7LUMQHRWTTILN7yR6LJwHR/tracks", Collections.emptyMap());
+        System.out.println(json);
+        System.out.println("Hello");
+    }
+
+    //TO DO
+    //List<Artist> getArtists(...) filtering by some criteria
+    //List<Album> getAlbums(artist)
+    //List<Track> getTracks(artist),
+
+}
