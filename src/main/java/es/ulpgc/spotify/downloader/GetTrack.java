9b047242bcb8ccebf98ba6e876b11135ececc800
eduardolfULPGC2003 <eduardolf2003@gmail.com>
2022-10-28 15:37:40 +0100

    GetTrack class updated

--- src/main/java/es/ulpgc/spotify/downloader/GetTrack.java
+++ src/main/java/es/ulpgc/spotify/downloader/GetTrack.java
@@ -1,4 +1,31 @@
 package es.ulpgc.spotify.downloader;
 
+import java.util.ArrayList;
+
 public class GetTrack {
+    private String href;
+    private ArrayList<Track> items;
+    private Integer limit;
+    private String next;
+    private Integer offset;
+    private String previous;
+    private Integer total;
+
+    public GetTrack(String href, ArrayList<Track> items, Integer limit, String next, Integer offset, String previous, Integer total) {
+        this.href = href;
+        this.items = items;
+        this.limit = limit;
+        this.next = next;
+        this.offset = offset;
+        this.previous = previous;
+        this.total = total;
+    }
+
+    public ArrayList<Track> getItems() {
+        return items;
+    }
+
+    public Integer getTotal() {
+        return total;
+    }
 }
