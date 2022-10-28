e4bbe55718fabbc9922afb9b10d3c095a7420129
eduardolfULPGC2003 <eduardolf2003@gmail.com>
2022-10-28 10:13:11 +0100

    Image class and Artist class updated

--- src/main/java/es/ulpgc/spotify/downloader/Artist.java
+++ src/main/java/es/ulpgc/spotify/downloader/Artist.java
@@ -87,29 +87,4 @@ public class Artist {
         }
     }
 
-    public class Image {
-        private String url;
-
-        private Integer height;
-
-        private Integer width;
-        public Image(String url, Integer height, Integer width) {
-            this.url = url;
-            this.height = height;
-            this.width = width;
-        }
-
-        public String getUrl() {
-            return url;
-        }
-
-        public Integer getHeight() {
-            return height;
-        }
-
-        public Integer getWidth() {
-            return width;
-        }
-    }
-
 }
--- src/main/java/es/ulpgc/spotify/downloader/Image.java
+++ src/main/java/es/ulpgc/spotify/downloader/Image.java
@@ -1,4 +1,26 @@
 package es.ulpgc.spotify.downloader;
 
 public class Image {
+    private String url;
+
+    private Integer height;
+
+    private Integer width;
+    public Image(String url, Integer height, Integer width) {
+        this.url = url;
+        this.height = height;
+        this.width = width;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
 }
