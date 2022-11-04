674266ef2764333dd6e4cfdf1bd8fc9168647f98
eduardolfULPGC2003 <eduardolf2003@gmail.com>
2022-11-04 12:27:51 +0000

    Updated commit 2

--- src/main/java/es/ulpgc/spotify/downloader/Main.java
+++ src/main/java/es/ulpgc/spotify/downloader/Main.java
@@ -13,27 +13,26 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         ArrayList<String> urlArtists = new ArrayList<>();
-        urlArtists.add("3oZa8Xs6IjlIUGLAhVyK4G");
+        //urlArtists.add("3oZa8Xs6IjlIUGLAhVyK4G");
         urlArtists.add("6qGkLCMQkNGOJ079iEcC5k");
-        urlArtists.add("4LbuSjHhhAddvN44qXpRJo");
-        urlArtists.add("5MmVJVhhYKQ86izuGHzJYA");
-        urlArtists.add("47BNWfpngeFHYvBlPPyraM");
-        urlArtists.add("6zFYqv1mOsgBRQbae3JJ9e");
-        urlArtists.add("7H55rcKCfwqkyDFH9wpKM6");
+        //urlArtists.add("4LbuSjHhhAddvN44qXpRJo");
+        //urlArtists.add("5MmVJVhhYKQ86izuGHzJYA");
+        //urlArtists.add("47BNWfpngeFHYvBlPPyraM");
+        //urlArtists.add("6zFYqv1mOsgBRQbae3JJ9e");
+        //urlArtists.add("7H55rcKCfwqkyDFH9wpKM6");
         SpotifyAccessor accessor = new SpotifyAccessor();
         //System.out.println(json);
         String dbPath = "C:\\Users\\Eduardo\\IdeaProjects\\spotify_2\\spotify.db";
         try(Connection conn = connect(dbPath)) {
             Statement statement = conn.createStatement();
-            statement.execute("CREATE TABLE IF NOT EXISTS artists (" +
-                    "ArtistID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            String columns = "ArtistID INTEGER PRIMARY KEY AUTOINCREMENT, " +
                     "Name TEXT NOT NULL, " +
                     "Followers INTEGER, " +
                     "Genres TEXT NOT NULL, " +
                     "Popularity INTEGER, " +
                     "ID TEXT NOT NULL, " +
-                    "Uri TEXT" +
-                    ")");
+                    "Uri TEXT";
+            Table.create("artists",columns,statement);
             statement.execute("CREATE TABLE IF NOT EXISTS albums (" +
                     "AlbumID INTEGER PRIMARY KEY AUTOINCREMENT, " +
                     "ArtistID INTEGER NOT NULL, " +
@@ -57,73 +56,23 @@ public class Main {
             for (String urlArtist : urlArtists) {
                 String json = accessor.get("/artists/" + urlArtist, Collections.emptyMap());
                 Artist artist = new Gson().fromJson(json, Artist.class);
-                statement.execute("INSERT INTO artists (Name,Followers,Genres,Popularity,ID,Uri) VALUES ('" +
-                        artist.getName() + "'," +
-                        artist.getFollower().getTotal() + "," +
-                        "'" + artist.getGenres().toString() + "'," +
-                        artist.getPopularity() + "," +
-                        "'" + artist.getId() + "'," +
-                        "'" + artist.getUri() + "'" +
-                        ")");
+                Table.insertArtist(artist,statement);
                 String json2 = accessor.get("/artists/" + urlArtist + "/albums?include_groups=album", Collections.emptyMap());
                 GetAlbum getAlbum = new Gson().fromJson(json2, GetAlbum.class);
                 ArrayList<Album> albums = getAlbum.getItems();
-                PreparedStatement preparedStatement = conn.prepareStatement("INSERT INTO albums (ArtistID,Name,ReleaseDate,TotalTracks,AvailableMarkets,ID,Uri) " +
-                        "VALUES (?,?,?,?,?,?,?)");
                 for (Album album : albums) {
                     System.out.println(new Gson().toJson(album));
                     ResultSet resultSet = statement.executeQuery("SELECT ArtistID FROM artists WHERE ID='" + artist.getId() + "'");
-                    preparedStatement.setInt(1,resultSet.getInt("ArtistID"));
-                    preparedStatement.setString(2,album.getName());
-                    preparedStatement.setString(3,album.getReleaseDate());
-                    preparedStatement.setInt(4,album.getTotalTracks());
-                    preparedStatement.setString(5,album.getAvailableMarkets().toString());
-                    preparedStatement.setString(6,album.getId());
-                    preparedStatement.setString(7,album.getUri());
-                    preparedStatement.execute();
-                    //preparedStatement.clearParameters();
-                    /*
-                    String secuencia = "INSERT INTO albums (ArtistID,Name,ReleaseDate,TotalTracks,AvailableMarkets,ID,Uri) VALUES ("+
-                            resultSet.getInt("ArtistID") + "," +
-                            "\"" + album.getName() + "\"," +
-                            "'" + album.getReleaseDate() + "'," +
-                            album.getTotalTracks() + "," +
-                            "'" + album.getAvailableMarkets().toString() + "'," +
-                            "'" + album.getId() + "'," +
-                            "'" + album.getUri() + "'" +
-                            ")";
-                    statement.execute(secuencia);
-                     */
+                    Table.insertAlbum(album, resultSet.getInt("ArtistID"), conn);
                     String idAlbum = album.getId();
                     String json3 = accessor.get("/albums/" + idAlbum + "/tracks", Collections.emptyMap());
                     GetTrack getTrack = new Gson().fromJson(json3, GetTrack.class);
                     ArrayList<Track> tracks = getTrack.getItems();
-                    PreparedStatement preparedStatement2 = conn.prepareStatement("INSERT INTO tracks (AlbumID,Name,TrackNumber,DurationMs,AvailableMarkets,ID,Uri) " +
-                            "VALUES (?,?,?,?,?,?,?)");
 
                     for (Track track : tracks) {
                         resultSet = statement.executeQuery("SELECT AlbumID FROM albums WHERE ID=\"" + album.getId() + "\"");
                         //System.out.println(new Gson().toJson(track));
-                        preparedStatement2.setInt(1,resultSet.getInt("AlbumID"));
-                        preparedStatement2.setString(2,track.getName());
-                        preparedStatement2.setInt(3,track.getTrackNumber());
-                        preparedStatement2.setInt(4,track.getDurationMs());
-                        preparedStatement2.setString(5,track.getAvailableMarkets().toString());
-                        preparedStatement2.setString(6,track.getId());
-                        preparedStatement2.setString(7,track.getUri());
-                        preparedStatement2.execute();
-                        //preparedStatement.clearParameters();
-                        /*
-                        statement.execute("INSERT INTO tracks (AlbumID,Name,TrackNumber,DurationMs,AvailableMarkets,ID,Uri) VALUES (" +
-                                resultSet.getInt("AlbumID") + "," +
-                                "\"" + track.getName() + "\"," +
-                                track.getTrackNumber() + "," +
-                                track.getDurationMs() + "," +
-                                "'" + track.getAvailableMarkets().toString() + "'," +
-                                "'" + track.getId() + "'," +
-                                "'" + track.getUri() + "'" +
-                                ")");
-                       */
+                        Table.insertTrack(track, resultSet.getInt("AlbumID"),conn);
                     }
 
 
@@ -162,4 +111,29 @@ public class Main {
     //for (Album album : albums) {
     //System.out.println(album.getId());
     //}
+
+    /*
+        statement.execute("INSERT INTO tracks (AlbumID,Name,TrackNumber,DurationMs,AvailableMarkets,ID,Uri) VALUES (" +
+            resultSet.getInt("AlbumID") + "," +
+            "\"" + track.getName() + "\"," +
+            track.getTrackNumber() + "," +
+            track.getDurationMs() + "," +
+            "'" + track.getAvailableMarkets().toString() + "'," +
+            "'" + track.getId() + "'," +
+            "'" + track.getUri() + "'" +
+        ")");
+   */
+
+    /*
+        String secuencia = "INSERT INTO albums (ArtistID,Name,ReleaseDate,TotalTracks,AvailableMarkets,ID,Uri) VALUES ("+
+                resultSet.getInt("ArtistID") + "," +
+                "\"" + album.getName() + "\"," +
+                "'" + album.getReleaseDate() + "'," +
+                album.getTotalTracks() + "," +
+                "'" + album.getAvailableMarkets().toString() + "'," +
+                "'" + album.getId() + "'," +
+                "'" + album.getUri() + "'" +
+                ")";
+        statement.execute(secuencia);
+     */
 }
--- src/main/java/es/ulpgc/spotify/downloader/Table.java
+++ src/main/java/es/ulpgc/spotify/downloader/Table.java
@@ -1,4 +1,50 @@
 package es.ulpgc.spotify.downloader;
 
+import javax.swing.plaf.nimbus.State;
+import java.sql.*;
+import java.sql.SQLException;
+
 public class Table {
+    public static void create(String name, String columns, Statement statement) throws SQLException {
+        statement.execute("CREATE TABLE IF NOT EXISTS " + name + "(" + columns + ")");
+    }
+
+    public static void insertArtist(Artist artist, Statement statement) throws SQLException {
+        statement.execute("INSERT INTO artists (Name,Followers,Genres,Popularity,ID,Uri) VALUES ('" +
+                artist.getName() + "'," +
+                artist.getFollower().getTotal() + "," +
+                "'" + artist.getGenres().toString() + "'," +
+                artist.getPopularity() + "," +
+                "'" + artist.getId() + "'," +
+                "'" + artist.getUri() + "'" +
+                ")");
+    }
+
+    public static void insertAlbum(Album album,Integer ArtistID,Connection conn) throws SQLException {
+        PreparedStatement preparedStatement = conn.prepareStatement("INSERT INTO albums (ArtistID,Name,ReleaseDate,TotalTracks,AvailableMarkets,ID,Uri) " +
+                "VALUES (?,?,?,?,?,?,?)");
+        preparedStatement.setInt(1,ArtistID);
+        preparedStatement.setString(2,album.getName());
+        preparedStatement.setString(3,album.getReleaseDate());
+        preparedStatement.setInt(4,album.getTotalTracks());
+        preparedStatement.setString(5,album.getAvailableMarkets().toString());
+        preparedStatement.setString(6,album.getId());
+        preparedStatement.setString(7,album.getUri());
+        preparedStatement.execute();
+        //preparedStatement.clearParameters();
+    }
+
+    public static void insertTrack(Track track, Integer AlbumID, Connection conn) throws SQLException {
+        PreparedStatement preparedStatement = conn.prepareStatement("INSERT INTO tracks (AlbumID,Name,TrackNumber,DurationMs,AvailableMarkets,ID,Uri) " +
+                "VALUES (?,?,?,?,?,?,?)");
+        preparedStatement.setInt(1,AlbumID);
+        preparedStatement.setString(2,track.getName());
+        preparedStatement.setInt(3,track.getTrackNumber());
+        preparedStatement.setInt(4,track.getDurationMs());
+        preparedStatement.setString(5,track.getAvailableMarkets().toString());
+        preparedStatement.setString(6,track.getId());
+        preparedStatement.setString(7,track.getUri());
+        preparedStatement.execute();
+        //preparedStatement.clearParameters();
+    }
 }
